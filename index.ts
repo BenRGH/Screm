22f532cdc8ebe1e9472b88b63287bdd4a2a21078
James Dibnah <jamesdibnah@gmail.com>
2022-05-22 13:52:54 +0100

    Adding new message trigger
    Changes:
     - Updated message trigger
     - Adding rule methods

--- index.ts
+++ index.ts
@@ -1,51 +1,51 @@
 import DiscordJs, {
     Intents,
-    MessageAttachment,
-    MessageEmbed,
+    Message,
+    MessageAttachment
 } from "discord.js";
 import dotenv from "dotenv";
+
 import { getRandomEmoticon, getRandomImageUrl } from "./utils";
 import { tomUrls } from "./Library/imgUrls";
 
+// Set up
 dotenv.config();
 
 const client = new DiscordJs.Client({
     intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES],
 });
 
-client.on("ready", () => {
-    console.log("Ready!");
-});
+// The rules
+const screamRules = ["scream", "screm", "scram"];
+const tomRules = ["tom", "tommy", "thomas"];
+const testRules = ["ping"];
 
-client.on("messageCreate", (message) => {
-    switch (message.content.toLowerCase()) {
-        case "screm":
-            message.channel.send({
-                content: getRandomEmoticon(),
-                files: [
-                    new MessageAttachment(getRandomImageUrl(), "screm.png"),
-                ],
-            });
-            break;
-        case "tom":
-        case "tommy":
-        case "thomas":
-            message.channel.send({
-                content: "meow?",
-                files: [
-                    new MessageAttachment(
-                        getRandomImageUrl(tomUrls),
-                        "tom.png"
-                    ),
-                ],
-            });
-            break;
-        case "ping":
-            message.reply({
-                content: "pong",
-                files: [],
-            });
-            break;
+// Checking if string contains any string held within the rules array.
+const ContainsRule = (str: string, rules: string[]) => rules.some(rule => str.toLowerCase().includes(rule));
+
+client.on("ready", () => console.log("Oh I am ready!"));
+
+client.on("messageCreate", (messageEvent:Message) => {
+    const message = messageEvent.content;
+    if (ContainsRule(message, screamRules)) {
+        messageEvent.channel.send({
+            content: getRandomEmoticon(),
+            files: [
+                new MessageAttachment(getRandomImageUrl(), "screm.png"),
+            ],
+        });
+    } else if (ContainsRule(message, tomRules)) {
+        messageEvent.channel.send({
+            content: "meow?",
+            files: [
+                new MessageAttachment(getRandomImageUrl(tomUrls), "tom.png"),
+            ],
+        });
+    } else if (ContainsRule(message, testRules)) {
+        messageEvent.reply({
+            content: "pong",
+            files: [],
+        });
     }
 });
 
