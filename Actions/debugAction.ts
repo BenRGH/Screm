68d6fba34838a9b8879d14ffa3763a952e7af55c
BenRGH <benjaminge.franklin@gmail.com>
2022-10-25 16:36:52 +0100

    tidy

--- Actions/debugAction.ts
+++ Actions/debugAction.ts
@@ -2,14 +2,13 @@ import { Message } from "discord.js";
 
 import {
     ContainsRule,
-    messageBuilder,
-    getGitCommit
+    messageBuilder
 } from "../utils";
 import Action from "./action";
 
 class DebugAction extends Action {
     constructor() {
-        super(["ping", "commit"]);
+        super(["ping"]);
     }
 
     match(message: string): boolean {
@@ -23,14 +22,6 @@ class DebugAction extends Action {
                 messageBuilder(
                     "pong",
                 )
-            );
-        } else if (ContainsRule(messageEvent.content, ["commit"])) {
-            const commit = await getGitCommit();
-            messageEvent.reply(
-                messageBuilder(JSON.stringify({
-                    author: commit.committer.name,
-                    message: commit.subject,
-                }))
             );
         }
     }
