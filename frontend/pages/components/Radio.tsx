0f3bc17fec1519d32863fc8a6e577210c27bfdbb
BenRGH <benjaminge.franklin@gmail.com>
2022-10-31 20:20:53 +0000

    finally tidy

--- frontend/pages/components/Radio.tsx
+++ frontend/pages/components/Radio.tsx
@@ -8,25 +8,25 @@ export interface IRadioItem {
 
 interface IRadioProps {
     items: IRadioItem[];
-    // onChange: (value: firebaseCollection) => void;
 }
 
 export default function Radio(props: IRadioProps) {
     return (
         <>
-            {props.items.map((item, index) => (
-                <div key={`radio-${index}`}>
-                    <label htmlFor="collection">{item.name}</label>
-                    <input
-                        type="radio"
-                        id={`collection-${index}`}
-                        name="collection"
-                        value={item.value}
-                        defaultChecked={index === 0}
-                        // onClick={() => props.onChange(item.value)}
-                    />
-                </div>
-            ))}
+            {props.items &&
+                props.items.length !== 0 &&
+                props.items.map((item, index) => (
+                    <div key={`radio-${index}`}>
+                        <label htmlFor="collection">{item.name}</label>
+                        <input
+                            type="radio"
+                            id={`collection-${index}`}
+                            name="collection"
+                            value={item.value}
+                            defaultChecked={index === 0}
+                        />
+                    </div>
+                ))}
         </>
     );
 }
